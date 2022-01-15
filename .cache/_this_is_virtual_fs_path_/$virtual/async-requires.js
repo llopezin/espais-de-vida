0db73d0e28dda88c949331a4aa5b1fb5d2111f7f
Laura <laura@HYD-Admins-MacBook-Pro.local>
2022-01-15 11:13:33 +0100

    menu icon positioning correction

--- .cache/_this_is_virtual_fs_path_/$virtual/async-requires.js
+++
@@ -1,8 +0,0 @@
-exports.components = {
-  "component---cache-dev-404-page-js": () => import("./../../dev-404-page.js" /* webpackChunkName: "component---cache-dev-404-page-js" */),
-  "component---src-pages-about-js": () => import("./../../../src/pages/about.js" /* webpackChunkName: "component---src-pages-about-js" */),
-  "component---src-pages-contact-js": () => import("./../../../src/pages/contact.js" /* webpackChunkName: "component---src-pages-contact-js" */),
-  "component---src-pages-index-js": () => import("./../../../src/pages/index.js" /* webpackChunkName: "component---src-pages-index-js" */),
-  "component---src-pages-reviews-js": () => import("./../../../src/pages/reviews.js" /* webpackChunkName: "component---src-pages-reviews-js" */)
-}
-
--- src/components/atoms/MenuIcon/MenuIcon.js
+++ src/components/atoms/MenuIcon/MenuIcon.js
@@ -5,6 +5,7 @@ const MenuIcon = () => {
   return (
     <div>
       <svg
+        className={style.menuIcon}
         xmlns="http://www.w3.org/2000/svg"
         version="1.1"
         width="32"
@@ -12,10 +13,7 @@ const MenuIcon = () => {
         viewBox="0 0 32 32"
       >
         <title>menu</title>
-        <path
-          className={style.menuIcon}
-          d="M2 6h28v4h-28zM2 14h28v4h-28zM2 22h28v4h-28z"
-        />
+        <path d="M2 6h28v4h-28zM2 14h28v4h-28zM2 22h28v4h-28z" />
       </svg>
     </div>
   );
