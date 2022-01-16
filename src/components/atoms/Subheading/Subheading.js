21373fe17d364799be8ea257214aec9dab54ee00
Laura <laura@HYD-Admins-MacBook-Pro.local>
2022-01-16 20:18:55 +0100

    subheading component

---
+++ src/components/atoms/Subheading/Subheading.js
@@ -0,0 +1,14 @@
+import React from "react";
+import * as style from "./Subheading.module.scss";
+import PropTypes from "prop-types";
+
+const Subheading = ({ type = "standard", text }) => {
+  return <h2 className={style[type]}>{text}</h2>;
+};
+
+export default Subheading;
+
+Subheading.propTypes = {
+  text: PropTypes.string,
+  type: PropTypes.oneOf(["main", "primary", "secondary"]),
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import SectionHeading from "../components/atoms/SectionHeading/SectionHeading";
 import BasicPageLayout from "../templates/BasicPageLayout";
+import Subheading from "../components/atoms/Subheading/Subheading";
 
 const home = () => {
   return (
     <BasicPageLayout>
       <section>
         <SectionHeading text="espacios de vida" type="main" />
+        <Subheading
+          text="CUÉNTAME QUÉ NECESITAS, YO LO ENCUENTRO"
+          type="main"
+        />
       </section>
     </BasicPageLayout>
   );
