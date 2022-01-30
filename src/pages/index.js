d57cfbc311c8baac3ea3ac54ef8b27e3bebd220a
Laura <laura@HYD-Admins-MacBook-Pro.local>
2022-01-30 13:37:32 +0100

    images

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,23 @@ import SectionHeading from "../components/atoms/SectionHeading/SectionHeading";
 import BasicPageLayout from "../templates/BasicPageLayout";
 import Subheading from "../components/atoms/Subheading/Subheading";
 import Separator from "../components/atoms/Separator/Separator";
-import SectionBody from "../components/molecules/SectionBody/SectionBody";
 import ComoFunciona from "../components/organisms/ComoFunciona/ComoFunciona";
 import TuEspacioDeVida from "../components/organisms/TuEspacioDeVida/TuEspacioDeVida";
+import MainImagesGrid from "../components/atoms/MainImagesGrid/MainImagesGrid";
+import Section from "../components/atoms/Section/Section";
 
 const home = () => {
   return (
     <BasicPageLayout>
-      <section>
+      <Section type='main'>
+        <MainImagesGrid />
         <SectionHeading text="espacios de vida" type="main" />
         <Separator length="lg" />
         <Subheading
           text="CUÉNTAME QUÉ NECESITAS, YO LO ENCUENTRO"
           type="main"
         />
-      </section>
+      </Section>
       <TuEspacioDeVida />
       <ComoFunciona />
     </BasicPageLayout>
