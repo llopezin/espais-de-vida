d8be836954449efb3ffefe436dc7dfbcb2d2aad2
Laura <laura@HYD-Admins-MacBook-Pro.local>
2022-02-01 20:21:38 +0100

    main images grid to use img over staticImage

--- src/components/atoms/MainImagesGrid/MainImagesGrid.js
+++ src/components/atoms/MainImagesGrid/MainImagesGrid.js
@@ -1,26 +1,17 @@
 import { StaticImage } from "gatsby-plugin-image";
 import React from "react";
 import * as style from "./MainImagesGrid.module.scss";
+import plant from "../../../images/plant.jpg"
+import table from "../../../images/table.jpg"
 
 const MainImagesGrid = () => {
   return (
     <div className={style.imagesWrap}>
       <div className={style.secondaryImageWrap}>
-      <StaticImage
-          src="../../../images/plant.jpg"
-          imgClassName={style.image}
-          objectFit="cover"
-        /> 
+        <img className={style.image} src={plant} alt=""/>
       </div>
       <div className={style.mainImageWrap}>
-        <StaticImage
-          src="../../../images/table.jpg"
-          imgClassName={style.mainImage}
-          objectFit="contain"
-          style={{
-            maxHeight: 500
-          }}
-        />
+        <img className={style.image} src={table} alt=""/>        
       </div>
     </div>
   );
