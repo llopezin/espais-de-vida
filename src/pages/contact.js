c88b5b9f28267263e14e6685d83ebb5552b4ea20
Laura <laura@HYD-Admins-MacBook-Pro.local>
2022-02-02 22:48:00 +0100

    updates to pages

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,14 @@
 import React from "react";
+import ContactForm from "../components/molecules/ContactForm/ContactForm";
 import BasicPageLayout from "../templates/BasicPageLayout";
 
 export default function Contact() {
   return (
     <BasicPageLayout>
-      <section>Contact</section>
+      <h1>Contact</h1>
+      <section>
+        <ContactForm />
+      </section>
     </BasicPageLayout>
   );
 }
