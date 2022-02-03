72cc9063d24ac9a8d0a29b86b7de5261b4b4b6cc
Laura <laura@HYD-Admins-MacBook-Pro.local>
2022-02-03 22:45:16 +0100

    contact heading

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,10 +1,13 @@
 import React from "react";
+import SectionHeading from "../components/atoms/SectionHeading/SectionHeading";
+import ContactForm from "../components/molecules/ContactForm/ContactForm";
 import BasicPageLayout from "../templates/BasicPageLayout";
 
 export default function Contact() {
   return (
     <BasicPageLayout>
-      <section>Contact</section>
+      <SectionHeading text="Contacto" />
+      <ContactForm />
     </BasicPageLayout>
   );
 }
