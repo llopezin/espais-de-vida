f142f75da2e7ce410822267dd2bae62c1cbd44fe
Laura <laura@HYD-Admins-MacBook-Pro.local>
2022-02-03 22:22:15 +0100

    form styled

--- src/components/molecules/ContactForm/ContactForm.js
+++ src/components/molecules/ContactForm/ContactForm.js
@@ -1,19 +1,45 @@
-import React, { useRef } from "react";
+import React from "react";
+import Button from "../../atoms/Button/Button";
+import Section from "../../atoms/Section/Section";
+import * as style from "./ContactForm.module.scss";
 
 const ContactForm = () => {
   return (
-    <form name="Contact Form" method="POST" data-netlify="true">
-      <input type="hidden" name="form-name" value="Contact Form" />
-      <div>
-        <label>Your Email:</label>
-        <input type="email" name="email" />
-      </div>
-      <div>
-        <label>Message:</label>
-        <textarea name="message" />
-      </div>
-      <button type="submit">Send</button>
-    </form>
+    <Section>
+      <form
+        className={style.contactForm}
+        name="Contact Form"
+        method="POST"
+        data-netlify="true"
+      >
+        <input type="hidden" name="form-name" value="Contact Form" />
+        <div>
+          <label>Tu nombre:</label>
+          <input type="name" name="name" required />
+          <span className={style.validation}></span>
+        </div>
+        <div>
+          <label>Email:</label>
+          <input
+            type="email"
+            name="email"
+            placeholder="ejemplo@gmail.com"
+            required
+          />
+        </div>
+        <div>
+          <label>Mensaje:</label>
+          <textarea
+            name="message"
+            placeholder="Cuéntame ¿En qué te puedo ayudar?"
+            required
+          />
+        </div>
+        <Button className="secondary" type="submit">
+          Enviar
+        </Button>
+      </form>
+    </Section>
   );
 };
 
