90a17ab6cf1e65d584778415c31d8964b2773da8
Laura Lopez <lauralopezinfantes@gmail.com>
2022-02-23 21:34:27 +0000

    site verification

--- src/components/atoms/Head/Head.js
+++ src/components/atoms/Head/Head.js
@@ -1,33 +1,37 @@
-import React from "react";
-import PropTypes from "prop-types";
-import { Helmet } from "react-helmet";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Helmet } from 'react-helmet'
 
 const Head = ({ title, description }) => {
   return (
     <Helmet
       htmlAttributes={{
-        lang: "en",
+        lang: 'en'
       }}
     >
-      <meta charSet="utf-8" />
+      <meta charSet='utf-8' />
       <title>{title}</title>
-      <meta name="description" content={description} />
+      <meta name='description' content={description} />
       <meta
-        name="keywords"
-        content="inmobiliaria, casas, chalet, valencia, javea, comprar"
+        name='keywords'
+        content='inmobiliaria, casas, chalet, valencia, javea, comprar'
       />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
       <meta
-        name="google-site-verification"
-        content="iR16DcmMB3-I8FIy3TIZsLiAQgipkqCu9Mk9kLSQWkg"
+        name='google-site-verification'
+        content='iR16DcmMB3-I8FIy3TIZsLiAQgipkqCu9Mk9kLSQWkg'
+      />
+      <meta
+        name='google-site-verification'
+        content='kb_XvjHLoiLusyJxH2hhUOVWA7S4LNwkNuHVQoxU1Xo'
       />
     </Helmet>
-  );
-};
+  )
+}
 
-export default Head;
+export default Head
 
 Head.propTypes = {
   title: PropTypes.string,
-  description: PropTypes.string,
-};
+  description: PropTypes.string
+}
