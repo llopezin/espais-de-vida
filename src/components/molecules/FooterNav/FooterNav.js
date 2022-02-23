fd85a5441c5b05065b44667956c9618b503d6081
Laura Lopez <lauralopezinfantes@gmail.com>
2022-02-23 22:33:21 +0000

    linkedin site linked

--- src/components/molecules/FooterNav/FooterNav.js
+++ src/components/molecules/FooterNav/FooterNav.js
@@ -1,33 +1,46 @@
-import React from "react";
-import EmailIcon from "../../atoms/EmailIcon/EmailIcon";
-import PhoneIcon from "../../atoms/PhoneIcon/PhoneIcon";
-import * as style from "./FooterNav.module.scss";
-import WhatsappIcon from "../../atoms/WhatsappIcon/WhatsappIcon";
+import React from 'react'
+import EmailIcon from '../../atoms/EmailIcon/EmailIcon'
+import PhoneIcon from '../../atoms/PhoneIcon/PhoneIcon'
+import * as style from './FooterNav.module.scss'
+import WhatsappIcon from '../../atoms/WhatsappIcon/WhatsappIcon'
+import LinkedInIcon from '../../atoms/LInkedInIcon/LInkedInIcon'
 
 const FooterNav = () => {
   return (
     <ul className={style.list}>
       <li>
-        <a className={style.link} href="mailto:nataliaferrus77@gmail.com">
+        <a className={style.link} href='mailto:nataliaferrus77@gmail.com'>
           <EmailIcon />
           <span>nataliaferrus77@gmail.com</span>
         </a>
       </li>
       <li>
-        <a className={style.link} href="https://wa.me/34605506068">
+        <a className={style.link} href='https://wa.me/34605506068'>
           <WhatsappIcon />
           <span>Contacta por whatsapp</span>
         </a>
       </li>
       <li>
-        <a className={style.link} href="tel:605506068">
+        <a className={style.link} href='tel:605506068'>
           <PhoneIcon />
-          <span>605506068</span>
-          <span className={style.small}>(A partir las 17:30)</span>
+          <div className={style.phoneWrap}>
+            <span>605506068</span>
+            <span className={style.small}>(A partir las 17:30)</span>
+          </div>
+        </a>
+      </li>
+      <li>
+        <a
+          className={style.link}
+          href='https://www.linkedin.com/company/espacios-de-vida'
+          target='_blank'
+        >
+          <LinkedInIcon />
+          <span>LinkedIn</span>
         </a>
       </li>
     </ul>
-  );
-};
+  )
+}
 
-export default FooterNav;
+export default FooterNav
