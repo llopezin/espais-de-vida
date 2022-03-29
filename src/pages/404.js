fda476d825be37f9c9d05b8515d600a81f22c0f8
Laura Lopez <lauralopezinfantes@gmail.com>
2022-03-29 14:42:31 +0200

    Create 404 page and block all pages

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SectionHeading from '../components/atoms/SectionHeading/SectionHeading'
+import Subheading from '../components/atoms/Subheading/Subheading'
+
+export default function () {
+  return (
+    <div className='center-message'>
+      <SectionHeading text='Pagina bajo mantenimiento' />
+      <Subheading text='Volveremos pronto' />
+    </div>
+  )
+}
